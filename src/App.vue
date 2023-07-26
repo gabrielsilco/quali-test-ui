fbb45dda65ffc96130c6b2989e739bf77f07e57b
gabrielsilco <gabriel.silco@gmail.com>
2023-07-26 19:39:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <PatientsList />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import PatientsList from './components/PatientsList.vue';
+</script>
---
+++ src/components/PatientsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+        <v-card max-width="300px" height="400px">Teste</v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('created')
+    },
+}
+</script>
